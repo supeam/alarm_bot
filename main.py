71fcd4864f2e1a4547cb6996ca070576f8509efb
supeam <rannakhot0908@gmail.com>
2025-08-01 16:36:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # รายชื่อสมาชิกที่สามารถจ่ายเงินได้
 members = {
     "lawganeyyeol.": "โฟม",
-    "inginging811": "อิง",
+    "inginging811 ": "อิง",
     "heart2952": "ฮาร์ท"
 }
 
